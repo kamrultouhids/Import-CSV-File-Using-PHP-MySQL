829eaaf2874589654db314c20667586552ccdaec
kamrultouhids <kamrultouhids@gmail.com>
2017-11-20 15:11:37 +0600

    Add files via upload

--- action.php
+++ action.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $db = "mydb";
+    $db = "pressto";
 
    $conn = mysqli_connect($servername, $username, $password, $db);
 
